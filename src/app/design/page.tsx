6b30b5d45a054ca23f5ad98d2f430405e41f39af
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2024-10-12 16:24:36 -0400

    basic page is now working

---
+++ src/app/design/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DesignPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Design Page</h1>
+      <p>This is the design page where you can showcase your design concepts.</p>
+      {/* Add more content related to design here */}
+    </div>
+  );
+};
+
+export default DesignPage;
